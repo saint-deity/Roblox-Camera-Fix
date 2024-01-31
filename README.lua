ff6b9e5d088e4070f2a5b7afc5107ccce6a4eacb
Deity <59446525+saint-deity@users.noreply.github.com>
2024-02-01 07:12:59 +1000

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,39 @@
+--[[
+-- 
+-- Deities Custom Camera
+-- https://github.com/saint-deity/Roblox-Camera-Fix/
+--
+--
+--
+-- ==MIT License==
+--
+-- Copyright (c) 2024 Deity
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--
+--
+--
+-- ==INSTRUCTIONS FOR USE==
+-- 
+-- PLACE THE SCRIPTS IN THE DIRECTORIES THAT ACCURATELY REPRESENT THE NAME OF THE FOLDER
+-- THAT YOU FIND THE SCRIPTS WITHIN (FIND THE DIRECTORIES UNDER THE STARTERPLAYER OBJECT).
+-- THE SCRIPTS ARE NOT INTENDED TO BE LEFT IN THEIR FOLDERS. YOU SHOULD MOVE ONLY THE
+-- SCRIPT OBJECTS. DELETE ALL THE FOLDERS ONCE THE SCRIPTS HAVE BEEN MOVED.
+-- 
+]]--
